5429663000491a9b530339ed1bde87c6c80a8d02
Sami <samsabeeh6@gmail.com>
2021-01-23 12:49:41 -0500

    commit tree a2a5be7f4a3f7c5b0b2f83d11fc0a1c6b4401775

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(number_given)
+    while number_given > 0 
+        puts "#{number_given} SECOND(S)!"
+        number_given -= 1 
+        
+    end 
+    "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number_given)
+    while number_given > 0 
+        puts "#{number_given} SECOND(S)!"
+        number_given -= 1 
+        sleep(1)  
+    end 
+end 
